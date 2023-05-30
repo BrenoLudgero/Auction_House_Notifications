a720c17c0dd557356aca931b750a74618012b7dd
BrenoLudgero <breno.ludgero@yahoo.com>
2023-05-30 17:02:38 -0300

    Add sound channel, ADDON_LOADED event

--- AH_Notifications.lua
+++ AH_Notifications.lua
@@ -1,16 +1,30 @@
 local frame = CreateFrame("FRAME")
 
-frame:RegisterEvent("CHAT_MSG_SYSTEM")
 
-print("Auction House Notifications loaded.")
+frame:RegisterEvent("CHAT_MSG_SYSTEM")
+frame:RegisterEvent("ADDON_LOADED")
+
 
 frame:SetScript("OnEvent", function(self, event, ...)
     local message = ...
 
-    if string.find(message, "Your auction of (.+) sold%.") then
-        PlaySound(891)
-    elseif string.find(message, "Your auction of (.+) has expired%.") then
-        PlaySound(892)
+    if event == "ADDON_LOADED" then
+
+        local addOnName = ...
+
+        if addOnName == "AH_Notifications" then
+            print("Auction House Notifications loaded successfully! (Version 0.3)")
+        end
+
+    elseif event == "CHAT_MSG_SYSTEM" then
+
+        if string.find(message, "Your auction of (.+) sold%.") then
+            PlaySound(891, "Master")
+
+        elseif string.find(message, "Your auction of (.+) has expired%.") then
+            PlaySound(892, "Master")
+        end
+
     end
 
 end)
