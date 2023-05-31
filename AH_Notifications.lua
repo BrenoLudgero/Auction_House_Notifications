eb9bef10cc57498e0b36a4dd153a5dba66f4d886
BrenoLudgero <breno.ludgero@yahoo.com>
2023-05-31 19:32:39 -0300

    Full multilingual support

--- AH_Notifications.lua
+++ AH_Notifications.lua
@@ -7,11 +7,23 @@ frame:RegisterEvent("ADDON_LOADED")
 
 successfulAuctionMessages = {"Your auction of (.+) sold%.", "You won an auction for (.+)", 
 "Has vendido (.+) en la subasta%.", "Has ganado una puja por (.+)", "Tu (.+) se ha vendido%.", 
-"Vente de votre (.+) réussie", "Vous avez gagné les enchères pour (.+)"}
+"Vente de votre (.+) réussie", "Vous avez gagné les enchères pour (.+)", 
+"Você vendeu (.+) no leilão%.", "Você ganhou um leilão de (.+)", 
+"Versteigerung: (.+) wurde verkauft%.", "Ihr habt eine Auktion gewonnen für: (.+)", 
+"На ваш товар (.+) нашелся покупатель%.", "(.+) предмет куплен. Вы выиграли торги%.", 
+"경매에 올린 (.+) 판매되었습니다%.", "(.+) 경매에 낙찰되었습니다%.", 
+"你拍賣的(.+)已經售出。", "你贏得了(.+)的競標", 
+"你拍卖的(.+)已经售出。", "你赢得了对(.+)的竞标"}
 
-failedAuctionMessages = {"Your auction of (.+) has expired%.", "You have been outbid on (.+)%.",
-"Tu subasta de (.+) ha caducado%.", "Alguien ha superado tu puja por (.+)%.", "Alguien ha pujado por tu (.+)%.",
-"Votre vente aux enchères de (.+) est terminée", "Vous n'êtes plus le plus offrant pour (.+)%."}
+failedAuctionMessages = {"Your auction of (.+) has expired%.", "You have been outbid on (.+)%.", 
+"Tu subasta de (.+) ha caducado%.", "Alguien ha superado tu puja por (.+)%.", "Alguien ha pujado por tu (.+)%.", 
+"Votre vente aux enchères de (.+) est terminée", "Vous n'êtes plus le plus offrant pour (.+)%.", 
+"O leilão de (.+) expirou%.", "Superaram seu lance em (.+)%.", 
+"Eure Auktion von (.+) ist abgelaufen%.", "Ihr wurdet bei (.+) überboten%.", 
+"Ваш товар (.+) снят с аукциона%.", "(.+) предмет перекуплен%.", 
+"(.+) 경매가 만료되었습니다%.", "(.+) 다른 플레이어에 의해 상회 입찰되었습니다%.", 
+"你拍賣的(.+)已經過期。", "你對(.+)的出價被人超過了。", 
+"你拍卖的(.+)已经过期。", "你对(.+)的出价被人超过了。"}
 
 
 frame:SetScript("OnEvent", function(self, event, ...)
@@ -23,7 +35,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
         local addOnName = ...
 
         if addOnName == "AH_Notifications" then
-            print("Auction House Notifications loaded successfully! (Version 0.5)")
+            print("Auction House Notifications loaded successfully! (Version 0.6)")
         end
 
     elseif event == "CHAT_MSG_SYSTEM" then
@@ -38,10 +50,10 @@ frame:SetScript("OnEvent", function(self, event, ...)
             end
         end
 
-        for _, pattern in ipairs(failedAuctionMessages) do              -- TEST
-            if string.find(message, pattern) then                       -- TEST
-                failedAuction = true                                    -- TEST
-                break                                                   -- TEST
+        for _, pattern in ipairs(failedAuctionMessages) do
+            if string.find(message, pattern) then
+                failedAuction = true
+                break
             end
         end
 
