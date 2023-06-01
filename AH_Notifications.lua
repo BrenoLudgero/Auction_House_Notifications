6656e2997ac1877ff36f999989ccaefe0d821da6
BrenoLudgero <breno.ludgero@yahoo.com>
2023-06-01 00:58:13 -0300

    Additional sounds, external AH messages

--- AH_Notifications.lua
+++ AH_Notifications.lua
@@ -1,31 +1,22 @@
 local frame = CreateFrame("FRAME")
 
 
+local sounds = {                    -- https://wow.tools/dbc/?dbc=filedata&build=6.0.1.18179#page=1
+    coins = {567483, 567501},       -- iMoneyDialogOpen, iMoneyDialogClose
+    female = {540628, 540560},      -- HumanFemaleCheer01, HumanFemale_err_lootdidntkill06
+    fireworks = {567011, 565499},   -- G_FireworkBoomGeneral5, G_BarrelExplodeCustom0
+    impact = {567912, 568128}       -- m2hSwordHitMetalShieldCrit, BlizzardImpact1f
+}
+
+
+local chosenSound = sounds.coins
+
+
 frame:RegisterEvent("CHAT_MSG_SYSTEM")
 frame:RegisterEvent("ADDON_LOADED")
 
 
-successfulAuctionMessages = {"Your auction of (.+) sold%.", "You won an auction for (.+)", 
-"Has vendido (.+) en la subasta%.", "Has ganado una puja por (.+)", "Tu (.+) se ha vendido%.", 
-"Vente de votre (.+) réussie", "Vous avez gagné les enchères pour (.+)", 
-"Você vendeu (.+) no leilão%.", "Você ganhou um leilão de (.+)", 
-"Versteigerung: (.+) wurde verkauft%.", "Ihr habt eine Auktion gewonnen für: (.+)", 
-"На ваш товар (.+) нашелся покупатель%.", "(.+) предмет куплен. Вы выиграли торги%.", 
-"경매에 올린 (.+) 판매되었습니다%.", "(.+) 경매에 낙찰되었습니다%.", 
-"你拍賣的(.+)已經售出。", "你贏得了(.+)的競標", 
-"你拍卖的(.+)已经售出。", "你赢得了对(.+)的竞标"}
-
-failedAuctionMessages = {"Your auction of (.+) has expired%.", "You have been outbid on (.+)%.", 
-"Tu subasta de (.+) ha caducado%.", "Alguien ha superado tu puja por (.+)%.", "Alguien ha pujado por tu (.+)%.", 
-"Votre vente aux enchères de (.+) est terminée", "Vous n'êtes plus le plus offrant pour (.+)%.", 
-"O leilão de (.+) expirou%.", "Superaram seu lance em (.+)%.", 
-"Eure Auktion von (.+) ist abgelaufen%.", "Ihr wurdet bei (.+) überboten%.", 
-"Ваш товар (.+) снят с аукциона%.", "(.+) предмет перекуплен%.", 
-"(.+) 경매가 만료되었습니다%.", "(.+) 다른 플레이어에 의해 상회 입찰되었습니다%.", 
-"你拍賣的(.+)已經過期。", "你對(.+)的出價被人超過了。", 
-"你拍卖的(.+)已经过期。", "你对(.+)的出价被人超过了。"}
-
-
+-- Split into multiple functions
 frame:SetScript("OnEvent", function(self, event, ...)
 
     local message = ...
@@ -35,7 +26,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
         local addOnName = ...
 
         if addOnName == "AH_Notifications" then
-            print("Auction House Notifications loaded successfully! (Version 0.6)")
+            print("Auction House Notifications loaded successfully! (Version 0.7)") -- Get from .toc
         end
 
     elseif event == "CHAT_MSG_SYSTEM" then
@@ -58,11 +49,11 @@ frame:SetScript("OnEvent", function(self, event, ...)
         end
 
         if successfulAuction then
-            PlaySound(891, "Master")
+            PlaySoundFile(chosenSound[1], "Master")
             successfulAuction = false
 
         elseif failedAuction then
-            PlaySound(892, "Master")
+            PlaySoundFile(chosenSound[2], "Master")
             failedAuction = false
         end
 
